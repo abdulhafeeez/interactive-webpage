11e17a3c1e401c0b772f3926faffbb422ddf8d7d
abdulhafeeez <falowoolamiposi@gmail.com>
2026-03-12 12:51:43 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("button").addEventListener("click", function(){
+    alert("HELLO! JAVASCRIPT REPORTING FOR DUTY!!");
+
+    document.body.style.background = "linear-gradient(to right, white, blue)";
+});
